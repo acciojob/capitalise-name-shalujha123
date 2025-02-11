5ca1051640385ca2cd73abdbe9a3387650a8e7cf
Shalu <88194182+shalujha123@users.noreply.github.com>
2025-02-11 20:39:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
-function capitaize(str){
+function capitalize(str){
 	let inputField = document.getElementById("fname")
 	inputField.value = inputField.value.toUpperCase()
 }
